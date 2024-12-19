b61712a1508dc4ffdf8bf2d007120ae6eebe680b
JotaScript <149014210+Johan409IA@users.noreply.github.com>
2024-12-19 08:29:11 -0500

    Add files via upload
    #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/JotaScript.java
@@ -0,0 +1,38 @@
+
+public class JotaScript {
+
+    // comentario simple
+    /* Comentario
+    en varias 
+    lineas
+     */
+// URL del sitio web
+// https://www.java.com/ 
+    
+    public static void main(String[] args) {
+
+        // variables
+        int mi_variable = 4;
+        String mi_otra_variable = "Hola Java";
+
+        // constantes
+        final double constante = 12.7;
+
+        // tipos de datos primitivos
+        byte dato1 = 100;     // dato entero de 8 bit
+        short dato2 = 14345;  // dato entero de 16 bit
+        int dato3 = 12354335; // dato entero de 36 bit
+        long dato4 = 123355476; // dato entero de 64 bit
+        double dato5 = 123.56;   // dato en decimal
+        float dato6 = 142.453f;   // dato en decimal de presicion simple
+        String dato7 = "Hola Java";  // cadena de texto
+        boolean dato8 = true;   // dato de tipo boolean
+        char dato9 = 'A';   // dato de tipo caracter
+
+        
+        // imprimir por terminal
+        System.out.println("Hola Java");
+
+    }
+
+}
